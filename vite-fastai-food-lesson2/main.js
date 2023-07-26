ca164a1c914bea0de69d16687293f3c90f81ef4a
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2023-07-26 19:36:21 +0530

    completed coding of single image upload and inference

--- vite-fastai-food-lesson2/main.js
+++ vite-fastai-food-lesson2/main.js
@@ -1,37 +1,39 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
 
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
 import { client } from "@gradio/client";
 
-const response_0 = await fetch("https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png");
-const exampleImage = await response_0.blob();
-						
-const app = await client("https://vyomthakkar-indian-food-classifier.hf.space/");
-const result = await app.predict("/predict", [
+const formEl = document.getElementById("input-img-form")
+const inputEl = document.getElementById("input-img")
+const imgEl = document.getElementById("input-img-display")
+const predSpaceEl = document.getElementById("pred-space")
+const predLabelEl = document.getElementById("pred-label")
+const predProbEl = document.getElementById("pred-prob")
+
+formEl.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	predLabelEl.textContent = ""
+	predProbEl.textContent = ""
+	predSpaceEl.style.display = "flex"
+	console.log(inputEl.files)
+	let url = window.URL.createObjectURL(inputEl.files[0]);
+	console.log(url)
+	imgEl.src = url
+	const response_0 = await fetch(url);
+	console.log(response_0)
+	const exampleImage = await response_0.blob();
+	console.log(exampleImage)
+	
+	const app = await client("https://vyomthakkar-indian-food-classifier.hf.space/");
+	const result = await app.predict("/predict", [
 				exampleImage, 	// blob in 'img' Image component
 	]);
 
-console.log(result.data);
+	console.log(result.data);
+
+	predLabelEl.textContent = result.data[0].label
+	const confidenceObj = result.data[0].confidences.filter((obj) => obj.label === result.data[0].label)[0]
+	console.log(confidenceObj)
+	predProbEl.textContent = `(${(confidenceObj.confidence * 100).toFixed(2) + "%"})`
+
+})
 
