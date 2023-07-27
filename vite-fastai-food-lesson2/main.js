64f4ac3f17a25cec3a36bb75370e51c91b3d77c9
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2023-07-27 12:54:58 +0530

    supports multiple image files as input

--- vite-fastai-food-lesson2/main.js
+++ vite-fastai-food-lesson2/main.js
@@ -11,32 +11,34 @@ formEl.addEventListener("submit", async (e) => {
 	predSpaceEl.style.display = "flex"
 	predSpaceEl.innerHTML = "<h3>Predicting...</h3>"
 	console.log(inputEl.files)
-	let url = window.URL.createObjectURL(inputEl.files[0]);
-	console.log(url)
-	const response_0 = await fetch(url);
-	console.log(response_0)
-	const exampleImage = await response_0.blob();
-	console.log(exampleImage)
-	
-	const app = await client("https://vyomthakkar-indian-food-classifier.hf.space/");
-	const result = await app.predict("/predict", [
-				exampleImage, 	// blob in 'img' Image component
-	]);
 
-	console.log(result.data);
-
-	const confidenceObj = result.data[0].confidences.filter((obj) => obj.label === result.data[0].label)[0]
-	console.log(confidenceObj)
-
-	predSpaceEl.innerHTML = `
-			<div class="pred-item">
-				<img class="input-img-display" src="${url}" />
-				<p class="pred-text">
-					<span class="pred-label">${result.data[0].label}</span>
-					<span class="pred-prob">(${(confidenceObj.confidence * 100).toFixed(2) + "%"})</span>
-				</p>
-			</div>
-	`
+	let predOut = ""
+	for (const file of inputEl.files) {
+		console.log(file)
+		const url = window.URL.createObjectURL(file);
+		console.log(url)
+		const response_0 = await fetch(url);
+		console.log(response_0)
+		const exampleImage = await response_0.blob();
+		console.log(exampleImage)
+		const app = await client("https://vyomthakkar-indian-food-classifier.hf.space/");
+		const result = await app.predict("/predict", [
+					exampleImage, 	// blob in 'img' Image component
+		]);
+		console.log(result.data);
+		const confidenceObj = result.data[0].confidences.filter((obj) => obj.label === result.data[0].label)[0]
+		console.log(confidenceObj)
+		predOut += `
+		<div class="pred-item">
+			<img class="input-img-display" src="${url}" />
+			<p class="pred-text">
+				<span class="pred-label">${result.data[0].label}</span>
+				<span class="pred-prob">(${(confidenceObj.confidence * 100).toFixed(2) + "%"})</span>
+			</p>
+		</div>
+		`
+	}
+	predSpaceEl.innerHTML = predOut
 
 })
 
