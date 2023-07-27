a297d2a0ecef18a19da08dc622612b5d02d6889f
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2023-07-27 15:14:54 +0530

    refactored html rendering code to employ createElement apis + textContent  instead of innerHtml. safer way to render, as it avoids potential malicious code injection vulnerabilities as a result of innerHtml

--- vite-fastai-food-lesson2/main.js
+++ vite-fastai-food-lesson2/main.js
@@ -6,39 +6,70 @@ const formEl = document.getElementById("input-img-form")
 const inputEl = document.getElementById("input-img")
 const predSpaceEl = document.getElementById("pred-space")
 
+//creates a dom element for a particular prediction object/unit (img + class + class prob)
+const createDomPredItem = (url, label, confidenceObj) => {
+
+	//create the parent pred item div that contains child items. this element will be eventually returned by the function
+	const predItemEl = document.createElement("div")
+	predItemEl.classList.add("pred-item")
+
+	//first child of pred item elem. imgEl contains the image on which to perform inference
+	const imgEl = document.createElement("img")
+	imgEl.classList.add("input-img-display")
+	imgEl.setAttribute("src", `${url}`)
+	predItemEl.appendChild(imgEl)
+
+	//second child of pred item. this item in turn contains 2 child span elements (pred label and preb prob). 
+	const predTextEl = document.createElement("p")
+	predTextEl.classList.add("pred-text")
+	const predLabelEl = document.createElement("span")
+	predLabelEl.classList.add("pred-label")
+	predLabelEl.textContent = label
+	const predProbEl = document.createElement("span")
+	predProbEl.classList.add("pred-prob")
+	predProbEl.textContent = `(${(confidenceObj.confidence * 100).toFixed(2) + "%"})`
+	predTextEl.appendChild(predLabelEl)
+	predTextEl.appendChild(predProbEl)
+	predItemEl.appendChild(predTextEl)
+
+	return predItemEl
+}
+
+//event listener that triggers when user submits the form
 formEl.addEventListener("submit", async (e) => {
 	e.preventDefault();
-	predSpaceEl.style.display = "flex"
+	predSpaceEl.style.display = "flex" //in css file pred-space has display set to none. when we need to make predictions, we set its display to flex
 	predSpaceEl.innerHTML = "<h3>Predicting...</h3>"
-	console.log(inputEl.files)
 
-	let predOut = ""
+	const predElems = []
+	//iterate through files(images) that have been submitted by the user
 	for (const file of inputEl.files) {
-		console.log(file)
 		const url = window.URL.createObjectURL(file);
-		console.log(url)
+		
 		const response_0 = await fetch(url);
-		console.log(response_0)
 		const exampleImage = await response_0.blob();
-		console.log(exampleImage)
+		
+		//user hf space endpoint for inference
 		const app = await client("https://vyomthakkar-indian-food-classifier.hf.space/");
 		const result = await app.predict("/predict", [
 					exampleImage, 	// blob in 'img' Image component
 		]);
-		console.log(result.data);
+
+		//extract label and confidence object from inference response
+		const label = result.data[0].label
 		const confidenceObj = result.data[0].confidences.filter((obj) => obj.label === result.data[0].label)[0]
-		console.log(confidenceObj)
-		predOut += `
-		<div class="pred-item">
-			<img class="input-img-display" src="${url}" />
-			<p class="pred-text">
-				<span class="pred-label">${result.data[0].label}</span>
-				<span class="pred-prob">(${(confidenceObj.confidence * 100).toFixed(2) + "%"})</span>
-			</p>
-		</div>
-		`
+
+		//create dom element from image url, label and confidence. will append to pred-space html element once all predictions are complete
+		const predItemEl = createDomPredItem(url, label, confidenceObj)
+		predElems.push(predItemEl)
+
 	}
-	predSpaceEl.innerHTML = predOut
+	
+	predSpaceEl.innerHTML = "" //clear Predicting... h3 element
+	//append predictItemEls to predSpaceEl html element
+	for (const el of predElems) {
+		predSpaceEl.appendChild(el)
+	}
 
 })
 
